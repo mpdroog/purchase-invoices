13ebc9804d5be7c5093745e517445731296433ad
mark <mpdroog@rootdev.nl>
2023-07-05 10:29:25 +0200

    Tooling. Set-up composer when composer.phar missing on root

--- import.sh
+++ import.sh
@@ -41,6 +41,26 @@ echo "year=$year quarter=$quarter"
 
 # cacert.pem
 wget -nv -O cacert.pem "https://curl.se/ca/cacert.pem"
+# composer.phar
+if [ ! -e "composer.phar" ]; then
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php composer-setup.php
+  php -r "unlink('composer-setup.php');"
+
+  cd transip
+  ../composer.phar install
+  cd -
+  cd digitalocean
+  ../composer.phar install
+  cd -
+  cd xsnews
+  ../composer.phar install
+  cd -
+  cd vultr
+  ../composer.phar install
+  cd -
+fi
 
 # Collect all invoices
 php transip/index.php -y=$year -q=$quarter $args
